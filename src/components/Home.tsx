0a0fcbc04407a757f84823dc3b1b39e1e80a5e38
Aminosyrer <90452342+Amiiiiiiiin@users.noreply.github.com>
2024-05-24 16:40:50 +0200

    Misc cleanup

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {  Spinner } from '@chakra-ui/react';
+import { Spinner, Box, Text } from '@chakra-ui/react';
 
 
 const Home: React.FC = () => {
 
     return (
-        <Spinner size="xl" />
+        <Box p={4}>
+            <Text
+                fontSize="xl" mb={4}>
+                Available appointments
+            </Text>
+            <Spinner size="xl" />
+        </Box>
     );
 };
 
